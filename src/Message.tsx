cba864d01b02d214209f5ed09e50f19cfe381013
sxv55110 <sxv55110@ucmo.com>
2025-04-07 02:18:35 +0530

    My react-app project

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+
+// PascalCasing
+function Message() {
+    const name = 'Dinesh';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
